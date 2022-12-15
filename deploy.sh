056df07628af4e0aa82df43ee073612a1f2dce1d
olawale afuye <walosha@yahoo.com>
2022-12-15 19:59:18 +0100

    typo

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 
 docker build -t walosha/multi-client:latest -t walosha/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t walosha/multi-server:latest -t walosha/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t walosha/multi-worker:latest -t walosha/multi-worker:$SHA -f ./worker/Dockerfile ./workers
+docker build -t walosha/multi-worker:latest -t walosha/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push walosha/multi-client:latest
 docker push walosha/multi-server:latest
